7d2d66683e8d25834dbcb4b2f9f1fbbfca54cf15
David Widmann <devmotion@users.noreply.github.com>
2021-10-08 18:34:38 +0200

    Simplify code and add documentation

--- generate_public.py
+++ generate_public.py
@@ -1,23 +1,20 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Export jupyter notebooks to html and a separate
-notebook without code striped.
+Export Jupyter notebook(s) to HTML and a cleaned notebook.
 
-To prevent a code cell from being emptied, add the tag 
-`keep` to the cell. Click View -> Cell Toolbar -> Tags.
+In the cleaned notebook,
+- cells tagged with 'remove' are removed (regardless of their type),
+- code cells tagged with 'keep' are copied,
+- and content of code cells without the tag 'keep' that is
+  enclosed by '#<keep>\n' and '#</keep>\n' is kept.
 
-
-To run from utils directory and export to ../build
-python generate_public.py -o .. ..
-
-Or to run from python directory
-python utils/generate_public.py .
-
-Export a single file
-python utils/generate_public.py session_2.py
+A code cell may contain multiple '#<keep>/n'-'#</keep>/n' blocks.
+At the end of a cell the newline character may be omitted, i.e. a
+cell may end with '#</keep>'.
 
 By: Johan Wågberg 2019
+Modified by: David Widmann 2021
 """
 import json
 import argparse
@@ -28,123 +25,112 @@ from nbconvert import HTMLExporter, PDFExporter, LatexExporter
 from nbconvert.writers import FilesWriter
 from bs4 import BeautifulSoup
 from pathlib import Path
-from itertools import tee
-
-def pairwise(iterable):
-    "s -> (s0,s1), (s1,s2), (s2, s3), ..."
-    a, b = tee(iterable)
-    next(b, None)
-    return zip(a, b)
-
-def export_html(file, path):
-    
-    with file.open() as f:
-        notebook = nbformat.read(f, as_version=4)
-        
-        c = Config()
-        c.FilesWriter.build_directory = path.parent.as_posix()
-           
-        for cell in notebook.cells:
-            if cell.get('cell_type') == 'code':
-                cell.source = re.sub('#<\/?keep>\n', '', cell.source)
-        
-        html_exporter = HTMLExporter(config=c)
-        html = html_exporter.from_notebook_node(notebook)
-        
-        soup = BeautifulSoup(html[0], 'html5lib')
-        
-        style = soup.new_tag('style')
-        style.attrs['type'] = 'text/css'
-        style.string = """.input .inner_cell .input_area pre {
-            -webkit-touch-callout: none;
-            -webkit-user-select: none;
-            -khtml-user-select: none;
-            -moz-user-select: none;
-            -ms-user-select: none;
-            user-select: none;
-        }"""
-        
-        soup.head.append(style)
-        
-        writer = FilesWriter(config=c)
-        writer.write(str(soup), *html[1:], path.stem)
-        print(f"Writing file { path }")
-
-def parse_cell(string):
-    # only keep content between '#<keep>\n' and '#</keep>\n' or '#</keep>$' (end of string)
-    regex = r'#<keep>\n(.*?)#</keep>(?:\n|$)'
-    return "".join(re.findall(regex, string, re.DOTALL))
-
-def parse_code_cell(cell):
-    if cell.execution_count:
-        cell.execution_count = None
-    if cell.outputs:
-        cell.outputs = []
-    tags = cell.get('metadata', {}).get('tags', [])
-    if not ('keep' in tags):
-        cell.source = parse_cell(cell.source)
 
 
-def export_without_code(file, path):
-    with file.open() as f:
-        notebook = nbformat.read(f, as_version=4)
-        new_cells = []
-        last_was_empty_code_cell = False
-        for cell in notebook.cells:
-            tags = cell.get('metadata', {}).get('tags', [])
-            if 'remove' in tags:
-                continue
-            
-            if cell.cell_type == 'code':
-                parse_code_cell(cell)
-                if not cell.source: # is empty
-                    if not last_was_empty_code_cell:
-                        new_cells.append(cell)
-                    last_was_empty_code_cell = True
-                else: # not empty
-                    new_cells.append(cell)
-                    last_was_empty_code_cell = False
-            else:
+def export_html(notebook, path):
+    """Export `notebook` as HTML file to `path`."""
+    c = Config()
+    c.FilesWriter.build_directory = path.parent.as_posix()
+
+    # remove '#<keep>' and '#</keep>'
+    for cell in notebook.cells:
+        if cell.get('cell_type') == 'code':
+            cell.source = re.sub('#<\/?keep>(?:\n|$)', '', cell.source)
+
+    # export notebook as HTML
+    html_exporter = HTMLExporter(config=c)
+    html = html_exporter.from_notebook_node(notebook)
+
+    # add CSS that makes it more difficult to copy solutions
+    soup = BeautifulSoup(html[0], 'html5lib')    
+    style = soup.new_tag('style')
+    style.attrs['type'] = 'text/css'
+    style.string = """.input .inner_cell .input_area pre {
+        -webkit-touch-callout: none;
+        -webkit-user-select: none;
+        -khtml-user-select: none;
+        -moz-user-select: none;
+        -ms-user-select: none;
+        user-select: none;
+    }"""
+    soup.head.append(style)
+
+    writer = FilesWriter(config=c)
+    writer.write(str(soup), *html[1:], path.stem)
+    print(f"Writing file { path }")
+
+
+def export_clean(notebook, path):
+    """Clean `notebook` and save it as `path`."""
+    new_cells = []
+    last_was_empty_code_cell = False
+    keep_regex = r'#<keep>\n(.*?)#</keep>(?:\n|$)'
+    for cell in notebook.cells:
+        # obtain and remove tags
+        tags = cell.get('metadata', {}).get('tags', [])
+        cell['metadata']['tags'] = []
+
+        # remove cells tagged with 'remove'
+        if 'remove' in tags:
+            continue
+
+        # copy other non-code cells
+        if cell.cell_type != 'code':
+            new_cells.append(cell)
+            last_was_empty_code_cell = False
+            continue
+
+        # handle remaining code cells
+        # remove execution count (the number on the left side) and output
+        if cell.execution_count:
+            cell.execution_count = None
+        if cell.outputs:
+            cell.outputs = []
+
+        if 'keep' in tags:
+            # copy code cell if it is tagged with 'keep'
+            new_cells.append(cell)
+            last_was_empty_code_cell = not cell.source
+        else:
+            # otherwise only keep content between '#<keep>\n' and '#</keep>\n'
+            # or '#</keep>$' (end of string)
+            cell.source = "".join(re.findall(keep_regex, cell.source, re.DOTALL))
+
+            # remove code cell completely if it is empty and the last cell was
+            # also an empty code cell
+            cell_is_empty = not cell.source
+            if not (cell_is_empty and last_was_empty_code_cell):
                 new_cells.append(cell)
-                last_was_empty_code_cell = False
-            cell['metadata']['tags'] = []
-                
-        notebook.cells = new_cells        
-        with path.open('w') as file:
-            json.dump(notebook, file)
-            print(f"Writing file {path}")
-        
-        
+                last_was_empty_code_cell = cell_is_empty
+
+    notebook.cells = new_cells        
+    with path.open('w') as file:
+        json.dump(notebook, file)
+        print(f"Writing file {path}")
+
+
 def get_parser():
     """Get parser object for generate_public.py"""
-   
     parser = argparse.ArgumentParser(description=__doc__,
                                      formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    
     parser.add_argument("path",
                         nargs='+',
-#                        required=True,
                         help="Path of a file or a folder of files")
     parser.add_argument("-e", "--extension",
                         default='ipynb',
                         help="File extension to filter by.")
-    
     parser.add_argument("-o", "--outdir",
                         default=".",
-                        help="Output directory.")
-    
-#    parser.add_argument("-r", "--recursive",
-#                        action="store_true",
-#                        default=False,
-#                        help="Search recursively in subfolders.")
-    
+                        help="Output directory (files are exported to subdirectory 'build').")
     return parser
-   
+
+
 def main():
+    # parse arguments
     args = get_parser().parse_args()
-    
+
+    # find notebooks
     paths = [Path(path) for path in args.path]
-    
     files = set()
     for path in paths:
         if path.is_file():
@@ -152,18 +138,25 @@ def main():
                 files.add(path)
         else:
             files |= set(path.glob(f"./*.{args.extension}"))
-#            if (args.recursive):
-#                paths += path.glob("*")
+
+    # define directory for exported files
     outdir = Path(args.outdir).resolve() / "build"
+
     for file in files:
         print(f"Processing {file.name}")
-            
+
+        # read notebook
+        with file.open() as f:
+            notebook = nbformat.read(f, as_version=4)
+
+        # export as HTML
         html_file = outdir / "solutions" / f"{file.stem}.html"
-        export_html(file, html_file)
-        
-        no_code_file = outdir / f"{file.stem}.{args.extension}"
-        export_without_code(file, no_code_file)
-    
+        export_html(notebook, html_file)
+
+        # export clean notebook
+        clean_file = outdir / f"{file.stem}.{args.extension}"
+        export_clean(notebook, clean_file)
+
+
 if __name__ == "__main__":
     main()
-    
